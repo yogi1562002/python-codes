c1cee95f83a84483e6ded0340217728188b003d8
yogi1562002 <nuluyogi@gmail.com>
2026-04-13 11:58:19 +0530

    Update alphabetordigit.py
    By using this code we will conclude weather a given character is number or alphabet or special character

--- alphabetordigit.py
+++ alphabetordigit.py
@@ -1,3 +1,4 @@
+# the code will says the given character is alphabet or number or special character 
 char = input("Enter the character here: ")
 if char.isalpha():
     print("the enter character is alphabet...")
